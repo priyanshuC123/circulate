300715cf31bae5461b0f10e276d9abe8f1f8214d
Priyanshu Choudhary <priyanschouhary100@gmail.com>
2024-08-25 20:26:52 +0530

    Add

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: Number(process.env.PORT) || 3000, // Convert process.env.PORT to a number
+    host: true, // Allow access from external IPs
+  },
+});
